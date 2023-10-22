c6d3388f9f05ad5d944eedc6b19a6d49a77e9410
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2023-10-22 11:18:06 +0300

    pkcs11 module priority updated

--- src/Network/PKCS11.cpp
+++ src/Network/PKCS11.cpp
@@ -12,12 +12,12 @@ PKCS11_CTX* ctx{ nullptr };
 std::vector<std::string> modules{
 	"C:\\Program Files\\SafeNet\\Authentication\\SAC\\x64\\IDPrimePKCS1164.dll",
 	"C:\\Windows\\System32\\OcsPKCS11Wrapper.dll",
-	"C:\\Windows\\System32\\cmP1164.dll",
+	"C:\\Windows\\System32\\idprimepkcs11.dll",
 	"C:\\Windows\\System32\\bit4ipki.dll",
 	"C:\\Windows\\System32\\cmP11.dll",
 	"C:\\Windows\\System32\\cvP11.dll",
 	"C:\\Windows\\System32\\siecap11.dll",
-	"C:\\Windows\\System32\\idprimepkcs11.dll"
+	"C:\\Windows\\System32\\cmP1164.dll"
 };
 
 
